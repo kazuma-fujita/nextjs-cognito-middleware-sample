c3c024e2031b68c823f19059a1ffc7c6d0de8ee9
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-12-01 16:57:02 +0900

    added amplify auth category

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nextjscognitomiddlew4549bf96": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
