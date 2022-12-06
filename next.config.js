19366dc0848d03b20ebe97540d9642852d253645
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-12-06 16:28:22 +0900

    added env object into next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    REGION: process.env.REGION,
+    COGNITO_USER_POOLS_ID: process.env.COGNITO_USER_POOLS_ID,
+    COGNITO_USER_POOLS_WEB_CLIENT_ID:
+      process.env.COGNITO_USER_POOLS_WEB_CLIENT_ID,
+    COGNITO_URL: process.env.COGNITO_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
